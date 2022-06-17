2a21bb7e81b035b836db94847e508b9085a093c2
sonal <imsonalbajaj@gmail.com>
2022-06-17 11:46:28 +0530

    L5-added cookies and session functionality

--- app.js
+++ app.js
@@ -4,31 +4,38 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
-var encrypt = require('mongoose-encryption');
-const bcrypt = require('bcrypt');
-
-const saltRounds = 10;
-mongoose.connect('mongodb://localhost:27017/userDB');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/userDB');
 const userSchemma = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchemma.plugin(encrypt, {
-    secret: process.env.SECRET,
-    encryptedFields: ["password"]
-});
-
+userSchemma.plugin(passportLocalMongoose);
 const User = new mongoose.model('user', userSchemma);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 ////////////////////    ROOT ROUTE     ////////////////////
 app.get('/', function (req, res) {
@@ -41,25 +48,23 @@ app.route('/login')
         res.render('login');
     })
     .post(function (req, res) {
-        const username = req.body.username;
-        const password = req.body.password;
-        User.findOne({
-            email: username
-        }, function (err, resp) {
-            if (!err) {
-                bcrypt.compare(password, resp.password, function (err, result) {
-                    if (result) res.render('secrets');
-                    else {
-                        console.log("Incorrect password");
-                        res.redirect('/');
-                    }
-                });
-            } else {
-                console.log(err);
-            }
-        })
-    })
 
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.login(user, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
+            }
+        });
+
+    })
 
 ////////////////////    REGISTER ROUTE  ////////////////////
 app.route('/register')
@@ -67,18 +72,35 @@ app.route('/register')
         res.render('register');
     })
     .post(function (req, res) {
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            const user = new User({
-                email: req.body.username,
-                password: hash
-            })
-            user.save(function (err, resp) {
-                if (!err) res.render('secrets');
-                else console.log(err);
-            })
+        User.register({
+            username: req.body.username
+        }, req.body.password, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function () {
+                    res.redirect("/secrets");
+                });
+            }
         });
-    })
 
+
+    });
+
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logout(function () {
+        res.redirect("/");
+    });
+});
 
 app.listen(PORT, () => {
     console.log("app is running on port %d", PORT);
